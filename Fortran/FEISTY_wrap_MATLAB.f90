183e2ad8b50683df162c6d05524fa66c58291d05
YixinZhaoGitHub <94640212+YixinZhaoGitHub@users.noreply.github.com>
2022-08-24 13:33:58 +0200

    Add files via upload

---
+++ Fortran/FEISTY_wrap_MATLAB.f90
@@ -0,0 +1,26 @@
+module FEISTY_wrap
+  use iso_c_binding
+  use FEISTY
+  use globals
+
+  implicit none
+
+contains
+
+subroutine f_setupVertical(pprod) bind(c)
+
+   real(c_double), intent(in), value :: pprod
+
+   call setupVertical(pprod)
+
+end subroutine f_setupVertical
+
+  subroutine f_calcderivatives(u, dudt) bind(c)
+    real(c_double), intent(in):: u(nGrid)
+    real(c_double), intent(out):: dudt(nGrid)
+
+    call calcderivatives(u, dudt)
+
+  end subroutine f_calcderivatives
+
+  end module FEISTY_wrap
