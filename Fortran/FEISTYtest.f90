7e0f50c631c5b7edb6ed405683df25fec13d14ad
YixinZhaoGitHub <94640212+YixinZhaoGitHub@users.noreply.github.com>
2022-09-07 15:00:59 +0200

    Update FEISTYtest.f90

--- Fortran/FEISTYtest.f90
+++ Fortran/FEISTYtest.f90
@@ -8,9 +8,10 @@ program FEISTYtest
    !real(dp),intent(out):: thetaF(nGrid,nGrid)
    !real(dp), allocatable:: flvl_r(:), mortpred_r(:), g_r(:)
 
-   call setupbasic(100.d0,5.d0)
+   !call setupbasic(100.d0,5.d0)
   !call setupbasic2(100.d0,5.d0,9)
    !call setupVertical(80.d0) ! ?
+   call setupsquid( 50.d0, 100.d0 , 6)
 
    allocate (u0(nGrid))
    allocate (dudt(nGrid))
@@ -18,14 +19,21 @@ program FEISTYtest
    !allocate (mortpred_r(nGrid))
    !allocate (g_r(nGrid-nResources))
 
-   u0(1) = 100.d0
-   u0(2) = 100.d0
-   u0(3) = 5.d0
+   u0(1) = 5.d0
+   u0(2) = 5.d0
+   u0(3) = 0.d0
    u0(4) = 0.d0
-   u0(idxF:nGrid) = 1.d0
+   u0(idxF:nGrid) = 0.0001d0
    dudt = 0.d0
 
-!! van Denderen et al., 2021
+!   u0(1) = 100.d0
+!   u0(2) = 100.d0
+!   u0(3) = 5.d0
+!   u0(4) = 0.d0
+!   u0(idxF:nGrid) = 1.d0
+!   dudt = 0.d0
+
+!! van Denderen et al., 2020
 !   u0(1) = 0.5d0
 !   u0(2) = 0.5d0
 !   u0(3) = 0.5d0
@@ -33,10 +41,9 @@ program FEISTYtest
 !   u0(idxF:nGrid) = 0.0001d0
 !   dudt = 0.d0
 
- call calcderivatives(u0,dudt)
+ !call calcderivatives(u0,dudt)
+ call calcderivativesSquid(u0,dudt)
 
    !call getrates(u0, dudt,flvl_r,mortpred_r,g_r)
-
-   print *, 'hello world'
 
 end program FEISTYtest
