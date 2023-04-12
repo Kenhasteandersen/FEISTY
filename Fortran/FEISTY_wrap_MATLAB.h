ae017851d683f1e5da34f0189f0266b6c58d8337
YixinZhaoGitHub <94640212+YixinZhaoGitHub@users.noreply.github.com>
2023-04-12 09:19:03 +0200

    Revert "Delete FEISTY_wrap_MATLAB.h"
    This reverts commit c4e89ce0d6b92eb6824bfe77a631657f41790daa.

---
+++ Fortran/FEISTY_wrap_MATLAB.h
@@ -0,0 +1,19 @@
+
+/* only lowercase
+
+*/
+
+void f_setupvertical(
+                const double szprod,
+                const double lzprod,
+                const int nStages,
+                const int region,
+                const double bottom,
+                const double photic);
+
+void f_calcderivatives(
+                const double u[],
+		        double dudt[]);
+
+
+
