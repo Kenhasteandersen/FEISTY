41880abd2083fc3adbd575d9e1e367d1b859c372
YixinZhaoGitHub <94640212+YixinZhaoGitHub@users.noreply.github.com>
2023-11-10 13:22:17 +0100

    Update Feisty_fishing.R

--- R/Feisty_fishing.R
+++ R/Feisty_fishing.R
@@ -16,3 +16,40 @@ setFishing = function(p, F=0, etaF=0.05) {
   }
   return(p)
 }
+
+
+#
+# Return the yield of all function groups
+#
+calcYield = function(
+    sim,          # The simulation object to analyse
+    etaTime=0.5) {# The fraction of the time series to integrate (default the last half) 
+  
+  p=sim$p
+  
+  yieldAllgrid = matrix(nrow=sim$nTime, ncol=length(p$ixFish))
+  yield = matrix(nrow=sim$nTime, ncol=p$nGroups)
+  yieldMean = rep(data=0, p$nGroups)
+  yieldMin = yieldMean
+  yieldMax = yieldMean
+  
+  ixTime = which(sim$t>=(etaTime*sim$t[sim$nTime]))
+  
+  for (iGroup in 1:p$nGroups) {
+    ix = p$ix[[iGroup]]    
+    yieldAllgrid[,ix-length(p$ixR)] =t(t(sim$u[, ix]) * p$mortF[ix]) 
+    yield[,iGroup]= rowSums(yieldAllgrid[,ix-length(p$ixR)])
+
+    #deltaM = p$mUpper[ix]-p$mLower[ix]
+    yieldMean[iGroup] = exp(mean(log(rowSums(yieldAllgrid[ixTime,ix-max(p$ixR)]+1e-10))))
+    yieldMin[iGroup] = min(rowSums( yieldAllgrid[ixTime,ix-max(p$ixR)] ))
+    yieldMax[iGroup] = max(rowSums( yieldAllgrid[ixTime,ix-max(p$ixR)] ))
+  }
+
+  sim$yieldMean=yieldMean
+  sim$yieldMin=yieldMin
+  sim$yieldMax=yieldMax
+  sim$yield2=yield
+  return(sim)
+}
+
