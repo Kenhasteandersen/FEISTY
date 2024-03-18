e2d99039758d0de98c2d2a484d48b85e6fbf56ff
kenhasteandersen <kha@aqua.dtu.dk>
2024-03-18 14:50:33 +0100

    Reordered items in the side bar

--- inst/webfeisty/server.R
+++ inst/webfeisty/server.R
@@ -75,57 +75,57 @@ server <- function(input, output) {
   
   sim <- eventReactive(
     c(
-    input$szprod,input$lzprod,input$bprodin,input$USEdll,input$Setup,input$nSizeGroups,
-    input$Tp,input$Tm,input$Tb,input$region, input$bottom,input$photic,input$etaMature,input$dfpho,
-    input$sh_de,input$F,input$etaF
-  ),
-  {
-    # setup simulation
-    if (input$Setup == "setupBasic") {
-      p = setupBasic(szprod = input$szprod, lzprod = input$lzprod, bprodin=input$bprodin,depth=input$sh_de,Tp=input$Tp,Tb=input$Tb)
-      #setupini = c(input$szprod,input$lzprod,input$bprod,input$sh_de,input$Tp,input$Tb)
-    }else if (input$Setup == "setupBasic2") {
-      p = setupBasic2(szprod = input$szprod, lzprod = input$lzprod, bprodin=input$bprodin,depth=input$sh_de,Tp=input$Tp,Tb=input$Tb,
-                      nStages =input$nSizeGroups, # Number of size groups
-                      etaMature=input$etaMature,
-                      F=input$F,
-                      etaF=input$etaF)
-      #setupini = c(input$szprod,input$lzprod,input$bprod,input$nSizeGroups,depth=input$sh_de,input$Tp,input$Tb,input$etaMature,input$F,input$etaF)
-    }else if (input$Setup == "setupVertical") {
-      p = setupVertical(szprod = input$szprod, lzprod = input$lzprod, dfpho=input$dfpho,
-                      #nStages  = input$nSizeGroups, # Number of size groups
-                      region   = as.integer(input$region),
-                      depth    = input$bottom,
-                      photic   = input$photic)
-      #setupini = c(input$szprod,input$lzprod,input$bent,input$nSizeGroups,input$region, input$bottom, input$photic)
-      
-    }else if (input$Setup == "setupVertical2") {
-      p = setupVertical2(szprod = input$szprod, lzprod = input$lzprod, dfpho=input$dfpho,
-                        nStages  = input$nSizeGroups, # Number of size groups
-                        Tp = input$Tp,
-                        Tm = input$Tm,
-                        Tb = input$Tb,
-                        depth    = input$bottom,
-                        photic   = input$photic,
-                        mesopelagic=250,
-                        visual=1.5,
+      input$szprod,input$lzprod,input$bprodin,input$USEdll,input$Setup,input$nSizeGroups,
+      input$Tp,input$Tm,input$Tb,input$region, input$bottom,input$photic,input$etaMature,input$dfpho,
+      input$sh_de,input$F,input$etaF
+    ),
+    {
+      # setup simulation
+      if (input$Setup == "setupBasic") {
+        p = setupBasic(szprod = input$szprod, lzprod = input$lzprod, bprodin=input$bprodin,depth=input$sh_de,Tp=input$Tp,Tb=input$Tb)
+        #setupini = c(input$szprod,input$lzprod,input$bprod,input$sh_de,input$Tp,input$Tb)
+      }else if (input$Setup == "setupBasic2") {
+        p = setupBasic2(szprod = input$szprod, lzprod = input$lzprod, bprodin=input$bprodin,depth=input$sh_de,Tp=input$Tp,Tb=input$Tb,
+                        nStages =input$nSizeGroups, # Number of size groups
                         etaMature=input$etaMature,
                         F=input$F,
                         etaF=input$etaF)
-      #setupini = c(input$szprod,input$lzprod,input$bent,input$nSizeGroups,input$region,input$bottom,input$photic,input$etaMature,input$F,input$etaF)
+        #setupini = c(input$szprod,input$lzprod,input$bprod,input$nSizeGroups,depth=input$sh_de,input$Tp,input$Tb,input$etaMature,input$F,input$etaF)
+      }else if (input$Setup == "setupVertical") {
+        p = setupVertical(szprod = input$szprod, lzprod = input$lzprod, dfpho=input$dfpho,
+                          #nStages  = input$nSizeGroups, # Number of size groups
+                          region   = as.integer(input$region),
+                          depth    = input$bottom,
+                          photic   = input$photic)
+        #setupini = c(input$szprod,input$lzprod,input$bent,input$nSizeGroups,input$region, input$bottom, input$photic)
+        
+      }else if (input$Setup == "setupVertical2") {
+        p = setupVertical2(szprod = input$szprod, lzprod = input$lzprod, dfpho=input$dfpho,
+                           nStages  = input$nSizeGroups, # Number of size groups
+                           Tp = input$Tp,
+                           Tm = input$Tm,
+                           Tb = input$Tb,
+                           depth    = input$bottom,
+                           photic   = input$photic,
+                           mesopelagic=250,
+                           visual=1.5,
+                           etaMature=input$etaMature,
+                           F=input$F,
+                           etaF=input$etaF)
+        #setupini = c(input$szprod,input$lzprod,input$bent,input$nSizeGroups,input$region,input$bottom,input$photic,input$etaMature,input$F,input$etaF)
+        
+      }
       
-    }
-
-    # Simulate
-    return( simulateFEISTY(bCust    = FALSE,
-                           p, 
-                           tEnd   = 100,
-                           tStep  = 1,
-                           times  = seq(from=0, to=200, by=1), #to=tEnd but must give a number directly
-                           yini   = p$u0,  
-                           USEdll = input$USEdll,
-                           Rmodel = derivativesFEISTYR) )
-  })
+      # Simulate
+      return( simulateFEISTY(bCust    = FALSE,
+                             p, 
+                             tEnd   = 100,
+                             tStep  = 1,
+                             times  = seq(from=0, to=200, by=1), #to=tEnd but must give a number directly
+                             yini   = p$u0,  
+                             USEdll = input$USEdll,
+                             Rmodel = derivativesFEISTYR) )
+    })
   
   # Make plots
   output$plotSimulation <- renderPlot( plotSimulation(sim()) )
