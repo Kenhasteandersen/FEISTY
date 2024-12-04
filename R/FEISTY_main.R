a7e8663527eaf9c7b396ded87c4aca1a0e72576b
YixinZhaoGitHub <94640212+YixinZhaoGitHub@users.noreply.github.com>
2024-12-04 11:34:03 +0100

    update timeseires functions

--- R/FEISTY_main.R
+++ R/FEISTY_main.R
@@ -643,3 +643,460 @@ simulateFEISTY = function(p      = setupBasic(),
   
   return(structure(sim, class = 'FEISTY'))
 }
+
+
+derivativesFEISTYR_ts = function(t,              # current time
+                                 u,              # all state variables
+                                 p,              # parameters
+                                 FullOutput=TRUE) {
+  p$Tp = p$getts(time=t,y=p$Tp_ts)
+  p$Tm = p$getts(time=t,y=p$Tm_ts)
+  p$Tb = p$getts(time=t,y=p$Tb_ts)
+  p$r[3] = p$getts(time=t,y=p$bprod_ts)
+  #p = paramAddPhysiology(p)
+  if(p$setup == "setupVertical2"){
+    p = paramTeffect_vet(p)
+  }else if(p$setup == "setupBasic" | p$setup == "setupBasic2"){
+    p = paramTeffect(p=p, # only for setupbasic & 2
+                     Tref=p$Tref,
+                     Q10=p$Q10,
+                     Q10m=p$Q10m,
+                     pelgroupidx=c(1:(p$nGroups-1)),
+                     demgroupidx=p$nGroups)  
+  }
+  
+  u[1]=p$getts(time=t,y=p$szbio_ts)
+  u[2]=p$getts(time=t,y=p$lzbio_ts)
+  u[3]=u[3]
+  
+  #print(p$Cmax[5])
+  print(t)
+  # split state variable vector into resource and fish
+  u[u<0]=0
+  R     = u[p$ixR]       # resource, prey
+  iFish = p$ixFish
+  B     = u[iFish]       # fish
+  
+  # update effective temperature for large demersal fish in shallow water
+  if(!is.null(p$depth) & !is.null(p$bET))
+    if (p$depth<200 & p$bET==TRUE) p=updateET(p=p,u=u)
+  
+  # ----------------------------------------------
+  # Consumption of all fish groups
+  # ----------------------------------------------
+  
+  # V: clearance rate, (m2/g/yr) 
+  # theta x u: prey available for consumption
+  # Encounter # /yr
+  Enc = p$V * (p$theta %*% u) 
+  
+  # ----------------------------------------------
+  # Predation mortality, /yr:
+  # = t(p$theta) %*% (f*p$Cmax/p$epsAssim*u/p$mc)
+  # ----------------------------------------------
+  #
+  mm = p$Cmax*p$V/(Enc+p$Cmax)*u # temporarily store
+  mm[ is.na(mm) ] = 0
+  mortpred = t(p$theta) %*% mm
+  
+  szprod = p$getts(time=t,y=p$szprod_ts)
+  lzprod = p$getts(time=t,y=p$lzprod_ts)
+  
+  dr_fac_theta = matrix(1, nrow = nrow(p$theta), ncol = ncol(p$theta)) 
+  if(mortpred[1]*u[1] > szprod | mortpred[2]*u[2] > lzprod){
+    
+    if (mortpred[1]*u[1] >= szprod) {
+      dr_fac_sz = szprod/(mortpred[1]*u[1])
+      dr_fac_theta[p$ixFish,1] = dr_fac_sz
+      mortpred[1]=dr_fac_sz*mortpred[1]
+    }
+    if (mortpred[2]*u[2] >= lzprod) {
+      dr_fac_lz = lzprod/(mortpred[2]*u[2])
+      dr_fac_theta[p$ixFish,2] = dr_fac_lz
+      mortpred[2]=dr_fac_lz*mortpred[2]
+    }
+  }
+  
+  # f: feeding level
+  # Cmax: maximum consumption rate, /yr
+  # f = Enc / (p$Cmax + Enc)
+  f   = (p$V * ((p$theta*dr_fac_theta) %*% u)) / (p$Cmax + Enc)   # Functional response
+  f[is.na(f)] = 0
+  
+  # net growth rate, /yr
+  Eavail  = p$epsAssim * p$Cmax * f - p$metabolism
+  
+  # ----------------------------------------------
+  # Total mortality (includes basal and fishing mortality)
+  # ----------------------------------------------
+  mort = mortpred + p$mort0 + p$mortF   # /year
+  
+  # ----------------------------------------------
+  # Derivative of fish groups
+  # ----------------------------------------------
+  
+  # Flux out of the size group
+  #------------------------------
+  v     = Eavail[iFish,]   # net growth rate
+  vplus = pmax(v,0)
+  
+  # fraction available for growth
+  kappa = 1 - p$psiMature[iFish]   
+  g     = kappa*vplus
+  
+  # growth to the next stage
+  gamma = (kappa*vplus - mort[iFish]) /
+    (1 - (1/p$z[iFish])^(1-mort[iFish]/(kappa*vplus)) )
+  
+  gamma[kappa==0] = 0 # No growth of fully mature classes
+  
+  # goes out of stage (size group)
+  Fout = gamma*B
+  
+  # Energy used for reproduction
+  Repro = p$psiMature[iFish]*vplus*B
+  
+  # Flux into the size group
+  #------------------------------
+  Fin = Fout*0
+  for (i in 1:p$nGroups) {
+    ix = p$ix[[i]] - p$ix[[1]][1] +1                  # growth to
+    ixPrev  = c(ix[length(ix)], ix[1:(length(ix)-1)]) # growth from
+    Fin[ix] = Fout[ixPrev]
+    
+    # for reproduction: consider the reproduction success
+    Fin[ix[1]] = p$epsRepro[i]*(Fin[ix[1]] + sum( Repro[ix] ))
+  }
+  
+  # ----------------------------------------------
+  # Assemble derivatives of fish:
+  # ----------------------------------------------
+  
+  dBdt = Fin - Fout + (v - mort[p$ixFish])*B - Repro
+  
+  # ----------------------------------------------
+  # Derivative of resources
+  # ----------------------------------------------
+  
+  # if (p$Rtype == 1)  # chemostat
+  #   dRdt = p$r*(p$K-R) - mortpred[p$ixR]*R
+  # else               # logistic
+  #   dRdt = p$r*R*(1-R/p$K) - mortpred[p$ixR]*R
+  dRdt = c(0,0,0,0)
+  dRdt[3] = p$r[3]*R[3]*(1-R[3]/p$K[3]) - mortpred[3]*R[3]
+  
+  # ----------------------------------------------
+  # Assemble output:
+  # ----------------------------------------------
+  if (FullOutput) { # Output everything
+    out = list()
+    out$deriv = c(dRdt, dBdt)
+    out$f     = f[-p$ixR,] # Feeding level only all fish stages, no resources
+    out$mortpred = mortpred[,]
+    out$g     = g # net growth rate fish stages
+    out$Repro = Repro
+    out$Fin   = Fin
+    out$Fout  = Fout
+    
+    # for the budget:
+    grazing = p$Cmax * f         # grazing rate, /yr
+    loss    = (1.-p$epsAssim) * grazing + p$metabolism # Energy loss to environments. Updated below.
+    Reprofrac = (1-kappa)*vplus
+    
+    il <- NULL
+    for (i in 1:length(p$ix)){
+      # Add the waste energy in reproduction of each stages. Note it does not include the waste energy from last stage energy flux out, added in `totLoss` below.
+      loss[p$ix[[i]]] = loss[p$ix[[i]]] + (1-p$epsRepro[i]) * Reprofrac[p$ix[[i]]-p$nResources] 
+      il <- c(il, rep(i, times=length(p$ix[[i]])))
+    }
+    
+    out$totMort    = tapply((mort   *u)[p$ixFish], INDEX=il, FUN=sum)
+    out$totGrazing = tapply((grazing*u)[p$ixFish], INDEX=il, FUN=sum)
+    # Add the waste energy in reproduction from flux out of the last stage of each functional type.
+    out$totLoss    = tapply((loss   *u)[p$ixFish], INDEX=il, FUN=sum) +(1-p$epsRepro)*Fout[sapply(p$ix, tail, n = 1)-p$nResources] 
+    out$totRepro   = tapply(Repro, INDEX=il, FUN=sum) + Fout[sapply(p$ix, tail, n = 1)-p$nResources] 
+    out$totRecruit = out$totRepro* p$epsRepro
+    out$totBiomass = tapply(B, INDEX=il, FUN=sum)
+    return(out)
+  }
+  else # Output just the derivatives
+    return( list(c(dRdt, dBdt)) )
+}
+
+
+# only setupBasic setupBasic2 setupVertical2
+simulateFEISTY_ts = function(p      = setupBasic(), 
+                             tEnd   = 1,
+                             tStep  = 1/12,
+                             times  = seq(from=0, to=tEnd, by=tStep),  
+                             yini   = p$u0,  
+                             USEdll = TRUE,
+                             Rmodel = derivativesFEISTYR_ts,
+                             bCust  = TRUE,
+                             spinup = T){
+  
+  nR      <- p$nResources[1]  # no of resources. [1] to make sure that this is only one number
+  nGroups <- p$nGroups[1] # no of fish groups
+  nGrid   <- p$nStages[1] # no of grid points
+  nFGrid  <- nGrid-nR # grid points of fish
+  
+  if (length(yini) != nGrid) 
+    stop ("length of 'yini' not ok - should be ", nGrid)  
+  
+  # Set tolerances for ode solving
+  rtol = 1E-8
+  atol = 1E-8
+  if (max(sapply(p$ix, length))>=21){    
+    rtol = 1E-10
+    atol = 1E-10}
+  if (max(sapply(p$ix, length))>27) stop("The size number cannot be more than 27 due to the low accuracy of integration.")
+  
+  
+  if (spinup == T) {
+    loopnum=4
+    timesspin=seq(from=0, to=0.1*tEnd, by=tStep)
+    pspin = p
+    pspin$getts=getts <- function(time, y) {
+      approxfun(x = timesspin, y = y, method = "constant", rule = 2, f = 0)(time)
+    }
+    pspin$szbio_ts = p$szbio_ts[1:(0.1*length(p$szbio_ts))]
+    pspin$szbio_ts[length(pspin$szbio_ts) + 1] = pspin$szbio_ts[length(pspin$szbio_ts)]
+    pspin$lzbio_ts = p$lzbio_ts[1:(0.1*length(p$lzbio_ts))]
+    pspin$lzbio_ts[length(pspin$lzbio_ts) + 1] = pspin$lzbio_ts[length(pspin$lzbio_ts)]
+    pspin$bprod_ts = p$bprod_ts[1:(0.1*length(p$bprod_ts))]
+    pspin$bprod_ts[length(pspin$bprod_ts) + 1] = pspin$bprod_ts[length(pspin$bprod_ts)]
+    pspin$szprod_ts = p$szprod_ts[1:(0.1*length(p$szprod_ts))]
+    pspin$szprod_ts[length(pspin$szprod_ts) + 1] = pspin$szprod_ts[length(pspin$szprod_ts)]
+    pspin$lzprod_ts = p$lzprod_ts[1:(0.1*length(p$lzprod_ts))]
+    pspin$lzprod_ts[length(pspin$lzprod_ts) + 1] = pspin$lzprod_ts[length(pspin$lzprod_ts)]
+    pspin$Tp_ts = p$Tp_ts[1:(0.1*length(p$Tp_ts))]
+    pspin$Tp_ts[length(pspin$Tp_ts) + 1] = pspin$Tp_ts[length(pspin$Tp_ts)]
+    pspin$Tm_ts = p$Tm_ts[1:(0.1*length(p$Tm_ts))]
+    pspin$Tm_ts[length(pspin$Tm_ts) + 1] = pspin$Tm_ts[length(pspin$Tm_ts)]
+    pspin$Tb_ts = p$Tb_ts[1:(0.1*length(p$Tb_ts))]
+    pspin$Tb_ts[length(pspin$Tb_ts) + 1] = pspin$Tb_ts[length(pspin$Tb_ts)]
+    for (i in 1:loopnum) {
+      u = ode(y=yini, times=timesspin, parms=pspin, func = Rmodel,
+              method = "ode45", rtol = rtol, atol = atol) #Run by R
+      yini = u[length(timesspin),c(p$ixR,p$ixFish)+1]
+      print(sprintf("i/loopnum = %.2f%%", 100*i/loopnum))
+    }
+    p$u0 = u[length(timesspin),c(p$ixR,p$ixFish)+1]
+    yini = p$u0
+  }
+  
+  p$getts=getts <- function(time, y) {
+    approxfun(x = times, y = y, method = "constant", rule = 2, f = 0)(time)
+  }
+  
+  # prepare output variable names
+  Sname <- p$stagenames
+  Fname <- p$stagenames[-(1:nR)]
+  Gname <- p$groupnames[-(1:nR)]
+  outnames <- c(
+    paste("f", Fname, sep="."), paste("mortpred", Sname, sep="."),
+    paste("g", Fname, sep="."), paste("Repro", Fname, sep="."),
+    paste("Fin", Fname, sep="."), paste("Fout", Fname, sep="."),
+    paste("totMort", Gname, sep="."), paste("totGrazing", Gname, sep="."),
+    paste("totLoss", Gname, sep="."), paste("totRepro", Gname, sep="."),
+    paste("totRecruit", Gname, sep="."), paste("totBiomass", Gname, sep="."))    
+  
+  #
+  # calculate in Fortran
+  #
+  
+  if (USEdll==TRUE){
+    
+    # names of functions in fortran code to be used
+    runfunc  <- "runfeisty"    # the derivative function
+    
+    if (bCust==TRUE) {    
+      # the integers to be passed to the fortran code
+      ipar <- c(nGroups,                           # total number of groups
+                nR,                                # total number of resources
+                unlist(lapply(p$ix, FUN=length)),  # number of stages per fish group
+                p$Rtype,                           # type of resource dynamics
+                if (is.null(p$pelgrididx)) 1 else length(p$pelgrididx), # length of pelgrididx. 1, if not defined.
+                if (is.null(p$pelgrididx)) 1 else p$pelgrididx, # all pelagic fish grid indices. 1, if not defined.
+                if (is.null(p$allgrididx)) 1 else length(p$allgrididx), # length of allgrididx. 1, if not defined.
+                if (is.null(p$allgrididx)) 1 else p$allgrididx, # all grid indices (resources+fish). 1, if not defined.
+                if (is.null(p$lgdemidx))   1 else length(p$lgdemidx), # length of lgdemidx. 1, if not defined.
+                if (is.null(p$lgdemidx))   1 else p$lgdemidx, # large demersal fish indices. 1, if not defined.
+                if (is.null(p$bET))        0 else as.integer(p$bET)) # effective temperature Boolean flag. 0 (FALSE), if not defined.
+      ipar <- as.integer(ipar)
+      if (length(c(nGroups,nR,unlist(lapply(p$ix, FUN=length)),p$Rtype)) != 3 + nGroups)
+        stop ("length of 'ipar' not ok; check parameters")
+      
+      if (any(dim(p$theta)-c(nGrid, nGrid) != 0))
+        stop ("dimension of 'theta' not ok: should be (", nGrid, ",", nGrid, ")")  
+      
+      # the double precision numbers to be passed to the fortran code
+      rpar   <- c(rep(p$K,  length.out=nR),            # resource parameters
+                  rep(p$r,  length.out=nR),  
+                  rep(p$epsRepro, length.out=nGroups), # group-specific parameter
+                  p$psiMature[-(1:nR)],                # fish-stage parameter
+                  p$z[-(1:nR)], 
+                  t(p$theta),                          # check if not transpose
+                  rep(p$epsAssim,   length.out=nGrid), # all  
+                  rep(p$V,          length.out=nGrid), 
+                  rep(p$Cmax,       length.out=nGrid),
+                  rep(p$metabolism, length.out=nGrid),
+                  rep(p$mort0,      length.out=nGrid),
+                  rep(p$mortF,      length.out=nGrid),
+                  rep(p$Vsave,          length.out=nGrid), 
+                  rep(p$Cmaxsave,       length.out=nGrid),
+                  rep(p$metabolismsave, length.out=nGrid),
+                  p$depth,
+                  p$Q10,
+                  p$Q10m,
+                  p$Tp,
+                  p$Tb)
+      
+      # 
+      # Run the simulation:
+      #
+      
+      initfunc <- "initfeisty"
+      
+      if (any(is.na(times)))  # one call and return
+        return( DLLfunc(y=yini, times=0, parms=NULL, dllname = "FEISTY",
+                        func=runfunc, initfunc=initfunc, outnames=outnames, nout=length(outnames),
+                        ipar=ipar, rpar=as.double(rpar)))
+      
+      u = ode(y=yini, times=times, parms=NULL, dllname = "FEISTY",
+              func=runfunc, initfunc=initfunc, outnames=outnames, nout=length(outnames),
+              ipar=ipar, rpar=as.double(rpar),
+              method = "ode45", rtol = rtol, atol = atol) # Run by dll
+    }
+    else
+    {     # for fixed setups
+      # Transmit input file path to Fortran library
+      passpath <- function() {
+        sys=Sys.info()['sysname']
+        
+        if (sys=='Darwin') {
+          sLibname = system.file("libs", "FEISTY.so", package = "FEISTY")
+        }
+        if (sys=='Linux') {
+          sLibname = system.file("libs", "FEISTY.so", package = "FEISTY")
+        }
+        if (sys=='Windows'){
+          if (Sys.info()['machine']=='x86-64'){
+            sLibname = system.file("libs/x64", "FEISTY.dll", package = "FEISTY")
+          }else{
+            sLibname = system.file("libs/i386", "FEISTY.dll", package = "FEISTY")
+          }
+        }
+        
+        # Reload dll to avoid crash?
+        if (is.loaded("setupbasic")) { # "setupbasic" is a function name of fortran dll
+          dyn.unload(sLibname)
+          dyn.load(sLibname)}
+        
+        file_path=system.file("extdata", "input.nml", package = "FEISTY")
+        dummy=.C("passpath", length=nchar(file_path), file_path_in = charToRaw(file_path))
+        file_path_V=system.file("extdata", "tempdata.dat", package = "FEISTY")
+        dummy=.C("passpathv", length=nchar(file_path_V), file_path_in=charToRaw(file_path_V))
+      }
+      
+      # Call the Fortran subroutine to pass input file path
+      passresult <- passpath()
+      
+      # Choose the setup:
+      if (p$setup=="setupBasic"){
+        initfunc <- "initfeistysetupbasic"
+        setupinput=c(p$szprod,p$lzprod,p$bprodin,p$dfbot,p$depth,p$Tp,p$Tb)
+      }else if(p$setup=="setupBasic2"){
+        initfunc <- "initfeistysetupbasic2"
+        setupinput=c(p$szprod,p$lzprod,p$bprodin,p$dfbot,length(p$ix[[p$nGroups]]),p$depth,p$Tp,p$Tb,p$etaMature,p$Fmax,p$etaF,as.integer(p$bET))
+      }else if(p$setup=="setupVertical"){
+        initfunc <- "initfeistysetupvertical"
+        setupinput = c(p$szprod,p$lzprod,p$bprodin,p$dfbot,p$dfpho,p$region, p$bottom, p$photic)
+      }else if(p$setup=="setupVertical2"){
+        initfunc <- "initfeistysetupvertical2"
+        setupinput = c(p$szprod,p$lzprod,p$bprodin,p$dfbot,p$dfpho,length(p$ix[[p$nGroups]]), p$Tp, p$Tm, p$Tb, p$bottom,p$photic,p$etaMature,
+                       p$shelfdepth,p$visual,p$Fmax,p$etaF)
+      }
+      
+      if (any(is.na(times)))  # one call and return
+        return( DLLfunc(y=yini, times=0, parms=as.double(setupinput), dllname = "FEISTY",
+                        func=runfunc, initfunc=initfunc, outnames=outnames, nout=length(outnames),
+                        ipar=NULL, rpar=NULL))
+      # Full simulation:
+      u = ode(y=yini, times=times, parms=as.double(setupinput), dllname = "FEISTY",
+              func=runfunc, initfunc=initfunc, outnames=outnames, nout=length(outnames),
+              ipar=NULL, rpar=NULL,
+              method = "ode45", rtol = rtol, atol = atol) # Run by dll
+    }   
+    #
+    # Calculate in R:
+    #
+  } else if (any(is.na(times))) {  # one call and return
+    return (Rmodel(0, yini, p))
+  } else {               # R-code
+    u = ode(y=yini, times=times, parms=p, func = Rmodel,
+            method = "ode45", rtol = rtol, atol = atol) #Run by R
+    # assign colnames
+    colnames(u)[(1+p$nStages+1):ncol(u)]=outnames
+  }
+  
+  #
+  # Assemble output:
+  #
+  u[,(1:2)+1]= cbind(p$szbio_ts,p$lzbio_ts)
+  sim   = list()
+  sim$u = u[,c(p$ixR,p$ixFish)+1]
+  sim$R = u[, p$ixR+1]
+  sim$B = u[, p$ixFish+1]
+  sim$t = times
+  sim$nTime = length(times)
+  sim$USEdll=USEdll
+  sim$p = p
+  
+  # feeding level
+  # "^xx" extracting data starts with "xx"
+  col_f=grep("^f.", colnames(u), value = TRUE)
+  sim$f=u[,col_f]  
+  # predation mortality rate
+  col_mortpred=grep("^mortpred", colnames(u), value = TRUE)
+  sim$mortpred=u[,col_mortpred]
+  # net growth rate
+  col_g=grep("^g.", colnames(u), value = TRUE)
+  sim$g=u[,col_g]
+  # Energy used for reproduction [g/m2/year]
+  col_Repro=grep("^Repro", colnames(u), value = TRUE)
+  sim$Repro=u[,col_Repro]
+  # Biomass flux into each size class
+  col_Fin=grep("^Fin", colnames(u), value = TRUE)
+  sim$Fin=u[,col_Fin]
+  # Biomass flux out of each size class
+  col_Fout=grep("^Fout", colnames(u), value = TRUE)
+  sim$Fout=u[,col_Fout]
+  # total mortality of each functional group [g/m2/year],which includes predation mortality, background mortality, and fishing mortality.
+  col_totMort=grep("^totMort", colnames(u), value = TRUE)
+  sim$totMort=u[,col_totMort]
+  # total grazing of each functional group [g/m2/year], Cmax * f (maximum consumption rate * feeding level), the food intake before assimilation.
+  col_totGrazing=grep("^totGrazing", colnames(u), value = TRUE)
+  sim$totGrazing=u[,col_totGrazing]
+  # total biomass loss of each functional group [g/m2/year], including unassimilated food and metabolism. They are released to environments.
+  col_totLoss=grep("^totLoss", colnames(u), value = TRUE)
+  sim$totLoss=u[,col_totLoss]
+  # total energy used for reproduction of each functional group [g/m2]
+  col_totRepro=grep("^totRepro", colnames(u), value = TRUE)
+  sim$totRepro=u[,col_totRepro]
+  # total recruitment of each functional group [g/m2], TotRecruit = TotRepro * epsRepro (reproduction efficiency)
+  col_totRecruit=grep("^totRecruit", colnames(u), value = TRUE)
+  sim$totRecruit=u[,col_totRecruit] 
+  # total biomass of each functional group [g/m2]
+  col_totBiomass=grep("^totBiomass", colnames(u), value = TRUE)
+  sim$totBiomass=u[,col_totBiomass]  
+  
+  #
+  # Calculate Spawning Stock Biomass and yield
+  #
+  sim=calcSSB(sim=sim,etaTime=0.4)
+  sim=calcYield(sim=sim,etaTime=0.4)
+  
+  return(structure(sim, class = 'FEISTY'))
+}
+
--- R/FEISTY_setup.R
+++ R/FEISTY_setup.R
@@ -1302,6 +1302,49 @@ setupVertical2 = function(szprod = 80, # small zoo production
   return(param)  
 }
 
+
+setupTimeseries = function (p = setupVertical2(),
+                            szbio_ts = Zbio/2, #c(1e3,1e3)
+                            lzbio_ts = Zbio/2,
+                            szprod_ts = Zhploss/2,
+                            lzprod_ts = Zhploss/2,
+                            bprodin_ts = NA, # benthos production
+                            dfbot_ts  = dfbot,#NA, # detrital flux reaching the bottom
+                            dfpho_ts  = NA, # detrital flux out of photic zone
+                            Tp_ts = Tp,
+                            Tm_ts = Tm,
+                            Tb_ts = Tb){ 
+  
+  p$szbio_ts = szbio_ts #seq(from=100, to=800, length.out=12)
+  p$szbio_ts[length(szbio_ts)+1] = szbio_ts[length(szbio_ts)] #p$zbio_ts[13] = 800
+  p$lzbio_ts = lzbio_ts
+  p$lzbio_ts[length(lzbio_ts)+1] = lzbio_ts[length(lzbio_ts)] 
+  p$szprod_ts = szprod_ts
+  p$szprod_ts[length(szprod_ts)+1] = szprod_ts[length(szprod_ts)]
+  p$lzprod_ts = lzprod_ts
+  p$lzprod_ts[length(lzprod_ts)+1] = lzprod_ts[length(lzprod_ts)] 
+  p$Tp_ts=Tp_ts
+  p$Tp_ts[length(Tp_ts)+1] = Tp_ts[length(Tp_ts)] 
+  p$Tm_ts=Tm_ts
+  p$Tm_ts[length(Tm_ts)+1] = Tm_ts[length(Tm_ts)] 
+  p$Tb_ts=Tb_ts
+  p$Tb_ts[length(Tb_ts)+1] = Tb_ts[length(Tb_ts)] 
+  #?????????????????????????????????
+  # benthic production calc
+  if (sum(all(!is.na(bprodin_ts)), all(!is.na(dfbot_ts)), all(!is.na(dfpho_ts)))>1) stop('Please check "bprodin_ts", "dfbot_ts" and "dfpho_ts" input. Only one of them should be assigned values, others should be kept as "NA".')
+  if (all(!is.na(bprodin_ts))) {bprod_ts = bprodin_ts} else {bprodin_ts = -1}
+  if (all(!is.na(dfbot_ts))) {bprod_ts = dfbot_ts*0.1} else {dfbot_ts = -1}
+  if (all(!is.na(dfpho_ts))) {bprod_ts = 0.1*(dfpho_ts*(depth/photic)^-0.86); if(bprod_ts>=0.1*dfpho_ts) bprod_ts=0.1*dfpho_ts} else {dfpho_ts = -1}
+  #?????????????????????????????????????  
+  p$bprod_ts=bprod_ts
+  p$bprod_ts[length(bprod_ts)+1] = bprod_ts[length(bprod_ts)]
+  p$K[3]=80
+  p$r[3]=bprod_ts[1]
+  
+  return(p)
+}
+
+
 # ------------------------------------------------------------------------------
 # Make a basic setup with just pelagic fish. Currently not functional
 #
