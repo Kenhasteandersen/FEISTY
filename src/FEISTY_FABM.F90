d6dbdb83bb137318e0285f6fbeba53a959b38738
kenhasteandersen <kha@aqua.dtu.dk>
2025-06-03 14:27:22 +0200

    update

--- src/FEISTY_FABM.F90
+++ src/FEISTY_FABM.F90
@@ -15,44 +15,35 @@ module FEISTY_FABM
    private
 
    type, extends(type_depth_integrated_particle), public :: type_feisty_fabm
-      ! State variables for fish (small pelagics, mesopelagics, large pelagics, 
-      ! mid-water predators, and demersal fish):
+      ! Add variable identifiers and parameters here.
+     ! type (type_bottom_state_variable_id),         allocatable :: id_u(:)
       type (type_bottom_state_variable_id),         allocatable :: id_smpel(:), id_mesopel(:), id_lgpel(:), id_midp(:), id_dem(:)
-      ! The vertical distributios of the fish:
       type (type_vertical_distribution_id),         allocatable :: id_smpel_w(:), id_mesopel_w(:), id_lgpel_w(:), id_midp_w(:), id_dem_w(:)
-      ! The benthos state variable:
       type (type_bottom_state_variable_id)                      :: id_benthos
-   
-      ! Dependency IDs for the state variables in the biogeochemical model:
-      ! Detritus (c, n, and p) (only used in the bottom grid cell):
-      type (type_state_variable_id)                             :: id_detritus_c, id_detritus_n, id_detritus_p
-      ! Small zooplankton carbon (c), nitrogen (n), and phosphorus (p):
-      ! (note that we register as a bottom variable, but it is actually summed over the water column)
-      type (type_bottom_dependency_id)                          :: id_smzoo_c, id_smzoo_n, id_smzoo_p
-      ! Large zooplankton carbon (c), nitrogen (n), and phosphorus (p):
+!      type (type_state_variable_id)                             :: id_detritus_c, id_detritus_n, id_detritus_p
+      type (type_bottom_dependency_id)                         :: id_smzoo_c, id_smzoo_n, id_smzoo_p
       !type (type_bottom_dependency_id)                         :: id_lgzoo_c,id_lgzoo_n,id_lgzoo_p
-      ! Temperature:
-      ! type (type_bottom_dependency_id)                           :: id_temp
+   ! type (type_bottom_dependency_id)                           :: id_temp
       
-      ! Dependency IDs for the outputs from FEISTY
-      ! Excretion of nitrogen (n) and phosphorus (p):
-      type (type_bottom_state_variable_id)                      :: id_excre_n, id_excre_p
-      ! Respiration (CO2 / DIC):
-      type (type_bottom_state_variable_id)                      :: id_respiration
-      ! Fecal pellets (for detritus / POM):
-      type (type_bottom_state_variable_id)                      :: id_feces
-      ! Carcasses:
-      type (type_bottom_state_variable_id)                      :: id_carcasses 
+      type (type_bottom_state_variable_id)                         :: id_excre_n, id_excre_p
+      type (type_bottom_state_variable_id)                         :: id_respiration_c
+      type (type_bottom_state_variable_id)                         :: id_feces_c, id_feces_n, id_feces_p
+      type (type_bottom_state_variable_id)                         :: id_carcasses_c, id_carcasses_n, id_carcasses_p
+  ! coupling
+      type (type_model_id)                                       :: id_zooplankton
       
-      ! Coupling; pointer to which model contains the zooplankton state varaiable
-      ! that we will integrater over the water column:
-      type (type_model_id)                                      :: id_zooplankton
       
    contains
       procedure :: initialize
       !  procedure :: do_bottom
       ! Reference model procedures here.
    end type type_feisty_fabm
+   
+    ! Redfieldian N:C and P:C ratios of predator biomass
+    real(rk), parameter :: NC = 16.0_rk/ 106.0_rk
+    real(rk), parameter :: PC = 1.0_rk / 106.0_rk
+    !from gww to C
+    real(rk), parameter :: gwwC = 1._rk/9._rk
 
 contains
 
@@ -64,6 +55,8 @@ contains
       real(rk)           :: szprod, lzprod, bprodin, dfbot, depth, Tp, Tb
       integer :: i
       character(len=10)  :: strindex
+      
+      class (type_vertical_depth_range), pointer :: depth_distribution
       
       ! Register model parameters  
       ! Fish pysilogical parameters. See input.nml in the original FEISTY R package and Fortran library
@@ -110,15 +103,114 @@ contains
       call setupbasic(szprod, lzprod, bprodin, dfbot, depth, Tp, Tb)
       
       ! Register state variables
-      allocate(self%id_u(nGrid))
-      call self%register_state_variable(self%id_u(1), 'u1', 'g m-2', 'biomass', initial_value=smz_ini, minimum=0.0_rk)
-      call self%register_state_variable(self%id_u(2), 'u2', 'g m-2', 'biomass', initial_value=lgz_ini, minimum=0.0_rk)
-      call self%register_state_variable(self%id_u(3), 'u3', 'g m-2', 'biomass', initial_value=smbent_ini, minimum=0.0_rk)
-      call self%register_state_variable(self%id_u(4), 'u4', 'g m-2', 'biomass', initial_value=lgbent_ini, minimum=0.0_rk)      
-      do i = idxF, nGrid
+      !allocate(self%id_u(nGrid))
+      !call self%register_state_variable(self%id_u(1), 'u1', 'g m-2', 'biomass', initial_value=smz_ini, minimum=0.0_rk)
+      !call self%register_state_variable(self%id_u(2), 'u2', 'g m-2', 'biomass', initial_value=lgz_ini, minimum=0.0_rk)
+      !call self%register_state_variable(self%id_u(3), 'u3', 'g m-2', 'biomass', initial_value=smbent_ini, minimum=0.0_rk)
+      !call self%register_state_variable(self%id_u(4), 'u4', 'g m-2', 'biomass', initial_value=lgbent_ini, minimum=0.0_rk)      
+
+      allocate(self%id_smpel(2))
+      allocate(self%id_smpel_w(2))! allpcate size of vertical distribution of small pelagics
+      do i = 1, 2 !small pelagic
          write (strindex,'(i0)') i
-         call self%register_state_variable(self%id_u(i), 'u'//trim(strindex), 'g m-2', 'biomass', initial_value=b_ini, minimum=0.0_rk)
+         call self%register_state_variable(self%id_smpel(i), 'smpel'//trim(strindex), 'g m-2', 'small pelagic biomass', initial_value=b_ini, minimum=0.0_rk)
+         call self%set_variable_property(self%id_smpel(i), 'disable_transport', .true.)
+         
+         call self%add_to_aggregate_variable(standard_variables%total_carbon, self%id_smpel(i), scale_factor = gwwC)
+         call self%add_to_aggregate_variable(standard_variables%total_nitrogen, self%id_smpel(i), scale_factor = gwwC*NC)
+         call self%add_to_aggregate_variable(standard_variables%total_phosphorus, self%id_smpel(i), scale_factor = gwwC*PC)
+         
+         call self%register_vertical_distribution(self%id_smpel_w(i),'smpel_w'//trim(strindex))
+         allocate(depth_distribution)
+         call self%add_child(depth_distribution, 'habitat_smpel_w'//trim(strindex))
+         call self%request_coupling('smpel_w'//trim(strindex), 'habitat_smpel_w'//trim(strindex)//'/w')
       end do
+
+      allocate(self%id_lgpel(3))
+      allocate(self%id_lgpel_w(3))
+      do i = 1, 3 !large pelagic
+         write (strindex,'(i0)') i
+         call self%register_state_variable(self%id_lgpel(i), 'lgpel'//trim(strindex), 'g m-2', 'large pelagic biomass', initial_value=b_ini, minimum=0.0_rk)
+         call self%set_variable_property(self%id_lgpel(i), 'disable_transport', .true.)
+         
+         call self%add_to_aggregate_variable(standard_variables%total_carbon, self%id_lgpel(i), scale_factor = gwwC)
+         call self%add_to_aggregate_variable(standard_variables%total_nitrogen, self%id_lgpel(i), scale_factor = gwwC*NC)
+         call self%add_to_aggregate_variable(standard_variables%total_phosphorus, self%id_lgpel(i), scale_factor = gwwC*PC)
+
+         call self%register_vertical_distribution(self%id_lgpel_w(i),'lgpel_w'//trim(strindex))
+         allocate(depth_distribution)
+         call self%add_child(depth_distribution, 'habitat_lgpel_w'//trim(strindex))
+         call self%request_coupling('lgpel_w'//trim(strindex), 'habitat_lgpel_w'//trim(strindex)//'/w')
+      
+      end do
+      
+      allocate(self%id_dem(3))
+      allocate(self%id_dem_w(3))
+      do i = 1, 3 !demersal fish
+         write (strindex,'(i0)') i
+         call self%register_state_variable(self%id_dem(i), 'dem'//trim(strindex), 'g m-2', 'demersal biomass', initial_value=b_ini, minimum=0.0_rk)
+         call self%set_variable_property(self%id_dem(i), 'disable_transport', .true.)
+         
+         call self%add_to_aggregate_variable(standard_variables%total_carbon, self%id_dem(i), scale_factor = gwwC)
+         call self%add_to_aggregate_variable(standard_variables%total_nitrogen, self%id_dem(i), scale_factor = gwwC*NC)
+         call self%add_to_aggregate_variable(standard_variables%total_phosphorus, self%id_dem(i), scale_factor = gwwC*PC)
+
+         call self%register_vertical_distribution(self%id_dem_w(i),'dem_w'//trim(strindex))
+         allocate(depth_distribution)         
+         call self%add_child(depth_distribution, 'habitat_dem_w'//trim(strindex))
+         call self%request_coupling('dem_w'//trim(strindex), 'habitat_dem_w'//trim(strindex)//'/w')      
+      end do
+      
+      
+      !benthos register
+      call self%register_state_variable(self%id_benthos,'benthos', 'g m-2', 'benthos biomass', initial_value=smbent_ini, minimum=0.0_rk)
+      
+       ! Depth-averaged dependencies
+       !call self%register_dependency(self%id_temp, 'temp', 'degrees_Celsius', 'depthaveraged temperature')
+       call self%register_dependency(self%id_smzoo_c, 'smzoo_c', 'mmol C m-2', 'depth-integrated small zooplankton carbon')
+       call self%register_dependency(self%id_smzoo_n, 'smzoo_n', 'mmol N m-2', 'depth-integrated small zooplankton nitrogen')
+       call self%register_dependency(self%id_smzoo_p, 'smzoo_p', 'mmol P m-2', 'depth-integrated small zooplankton phosphorus') 
+       
+       
+       call self%register_state_dependency(self%id_excre_n,'excretion_N', 'mmol N m-2', 'excretion nitrogen')
+       call self%register_state_dependency(self%id_excre_p,'excretion_p', 'mmol C m-2', 'excretion phosphorus')
+       
+       call self%register_state_dependency(self%id_respiration_c,'respiration_c', 'mmol C m-2', 'respiration carbon')
+       
+       call self%register_state_dependency(self%id_feces_c,'feces_c', 'mmol C m-2', 'feces carbon')
+       call self%register_state_dependency(self%id_feces_n,'feces_n', 'mmol N m-2', 'feces nitrogen')
+       call self%register_state_dependency(self%id_feces_p,'feces_p', 'mmol P m-2', 'feces phosphorus')
+
+       call self%register_state_dependency(self%id_carcasses_c,'carcasses_c', 'mmol C m-2', 'carcasses carbon')
+       call self%register_state_dependency(self%id_carcasses_n,'carcasses_n', 'mmol N m-2', 'carcasses nitrogen')
+       call self%register_state_dependency(self%id_carcasses_p,'carcasses_p', 'mmol P m-2', 'carcasses phosphorus')
+      
+       
+       call self%request_mapped_coupling_to_model(self%id_smzoo_c, 'small zooplankton',standard_variables%total_carbon)
+       call self%request_mapped_coupling_to_model(self%id_smzoo_n, 'small zooplankton',standard_variables%total_nitrogen)
+       call self%request_mapped_coupling_to_model(self%id_smzoo_p, 'small zooplankton',standard_variables%total_phosphorus)
+
+       call self%request_mapped_coupling_to_model(self%id_excre_n, 'excretion',standard_variables%total_nitrogen)
+       call self%request_mapped_coupling_to_model(self%id_excre_p, 'excretion',standard_variables%total_phosphorus)  
+       
+       call self%request_mapped_coupling_to_model(self%id_feces_c, 'feces',standard_variables%total_carbon)
+       call self%request_mapped_coupling_to_model(self%id_feces_n, 'feces',standard_variables%total_nitrogen)
+       call self%request_mapped_coupling_to_model(self%id_feces_p, 'feces',standard_variables%total_phosphorus)
+       
+       call self%request_mapped_coupling_to_model(self%id_carcasses_c, 'carcasses',standard_variables%total_carbon)
+       call self%request_mapped_coupling_to_model(self%id_carcasses_n, 'carcasses',standard_variables%total_nitrogen)
+       call self%request_mapped_coupling_to_model(self%id_carcasses_p, 'carcasses',standard_variables%total_phosphorus)
+       
+       call self%register_mapped_model_dependency(self%id_zooplankton, 'small zooplankton', proportional_change=.true., domain=domain_bottom)
+       
+       
+       
+      
+!      do i = idxF, nGrid
+!         write (strindex,'(i0)') i
+!         call self%register_state_variable(self%id_u(i), 'u'//trim(strindex), 'g m-2', 'biomass', initial_value=b_ini, minimum=0.0_rk)
+!               call self%set_variable_property(self%id_u(i), 'disable_transport', .true.)
+!      end do
       
    end subroutine initialize
 
