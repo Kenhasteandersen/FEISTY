deaec4dab1edf73393c43cee26314c70ae28ee92
kenhasteandersen <kha@aqua.dtu.dk>
2025-06-03 14:36:46 +0200

    update

--- src/FEISTY_FABM.F90
+++ src/FEISTY_FABM.F90
@@ -38,7 +38,7 @@ module FEISTY_FABM
       ! Fecal pellets (for detritus / POM):
       type (type_bottom_state_variable_id)                      :: id_feces_c, id_feces_n, id_feces_p
       ! Carcasses:
-      type (type_bottom_state_variable_id)                      :: id_carcasses, id_carcasses_n, id_carcasses_p 
+      type (type_bottom_state_variable_id)                      :: id_carcasses_c, id_carcasses_n, id_carcasses_p 
  
       ! Coupling; pointer to which model contains the zooplankton state varaiable
       ! that we will integrater over the water column:
